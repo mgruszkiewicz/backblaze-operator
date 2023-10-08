12ca7127193b110ff013d1f507b30755a84f0707
issei <issei@issei.space>
2023-10-08 13:22:10 +0200

    :sparkles: fixing creating and deleting buckets

--- controllers/bucket_controller.go
+++ controllers/bucket_controller.go
@@ -60,9 +60,8 @@ func (r *BucketReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 
 	log := r.Log.WithValues("bucket", req.NamespacedName)
 	bucket := &b2v1alpha1.Bucket{}
-	err := r.Get(ctx, types.NamespacedName{Name: req.Name, Namespace: req.Namespace}, bucket)
 
-	if err != nil {
+	if err := r.Get(ctx, types.NamespacedName{Name: req.Name, Namespace: req.Namespace}, bucket); err != nil {
 		if errors.IsNotFound(err) {
 			// object not found, could have been deleted after
 			// reconcile request, hence don't requeue
@@ -82,85 +81,82 @@ func (r *BucketReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 		log.Info("Bucket is being deleted")
 		return r.reconcileDelete(ctx, bucket)
 	}
-
 	r.reconcileCreate(ctx, bucket)
 
 	return ctrl.Result{}, nil
 }
 
 func (r *BucketReconciler) reconcileCreate(ctx context.Context, bucket *b2v1alpha1.Bucket) (ctrl.Result, error) {
-	log := r.Log.WithValues("bucket", bucket.Namespace)
-
-	// Checking if backblaze secrets are set
-	if os.Getenv("B2_APPLICATION_ID") == "" || os.Getenv("B2_APPLICATION_KEY") == "" {
-		log.Info("B2_APPLICATION_ID or B2_APPLICATION_KEY not set")
+	// Create or update the bucket
+	if err := r.createOrUpdateBucket(ctx, bucket); err != nil {
+		return ctrl.Result{}, err
 	}
-	// Initializing backblaze client
-	b2, _ := backblaze.NewB2(backblaze.Credentials{
-		AccountID:      os.Getenv("B2_APPLICATION_ID"),
-		ApplicationKey: os.Getenv("B2_APPLICATION_KEY"),
-	})
-	// Define bucket ACL
-	bucket_acl := backblaze.AllPrivate
-	switch bucket.Spec.Acl {
-	case "private":
-		bucket_acl = backblaze.AllPrivate
-	case "public":
-		bucket_acl = backblaze.AllPublic
-	}
-	// Creating Bucket
-	bucket_b2, bucket_err := b2.CreateBucket(bucket.Name, bucket_acl)
-
-	if bucket_err != nil {
-		log.Info("error occured")
-		fmt.Println(bucket_err)
-	} else {
-		bucket.Status.AtProvider = true
-	}
-
-	fmt.Println(string(bucket_b2.BucketType))
-	l := r.Log.WithValues("bucket", bucket.Namespace)
-	l.Info("Creating deployment")
-
-	// Create or update the deployment
-	// if err := r.createOrUpdateDeployment(ctx, bucket); err != nil {
-	// 	return ctrl.Result{}, err
-	// }
 
 	return ctrl.Result{}, nil
 }
 
-// func (r *BucketReconciler) createOrUpdateDeployment(ctx context.Context, bucket *b2v1alpha1.Bucket) error {
-// 	deplName := types.NamespacedName{Name: app.ObjectMeta.Name + "-deployment", Namespace: app.ObjectMeta.Name}
+func (r *BucketReconciler) createOrUpdateBucket(ctx context.Context, bucket *b2v1alpha1.Bucket) error {
+	log := r.Log.WithValues("bucket", bucket.Namespace)
+	// Check if the bucket exists
+	if err := r.Get(ctx, types.NamespacedName{Name: bucket.Name, Namespace: bucket.Namespace}, bucket); err != nil {
+		if !errors.IsNotFound(err) {
+			return fmt.Errorf("Unable to fetch Bucket: %v", err)
+		}
+	}
 
-// 	// Check if the deployment exists
-// 	if err := r.Get(ctx, deplName, &depl); err != nil {
-// 		if !apierrors.IsNotFound(err) {
-// 			return fmt.Errorf("Unable to fetch Deployment: %v", err)
-// 		}
+	// If bucket was not already reconciled (most likely new CR)
+	if !bucket.Status.Reconciled {
+		// Creating new bucket
 
-// 		// Create a new deployment
-// 		depl = appsv1.Deployment{
-// 			// ...
-// 			// Define the deployment specification here
-// 			// ...
-// 		}
-// 		if err := r.Create(ctx, &depl); err != nil {
-// 			return fmt.Errorf("Unable to create Deployment: %v", err)
-// 		}
-// 		return nil
-// 	}
+		// Checking if backblaze secrets are set
+		if os.Getenv("B2_APPLICATION_ID") == "" || os.Getenv("B2_APPLICATION_KEY") == "" {
+			log.Info("B2_APPLICATION_ID or B2_APPLICATION_KEY not set")
+		}
+		// Initializing backblaze client
+		b2, _ := backblaze.NewB2(backblaze.Credentials{
+			AccountID:      os.Getenv("B2_APPLICATION_ID"),
+			ApplicationKey: os.Getenv("B2_APPLICATION_KEY"),
+		})
+		// Define bucket ACL
+		bucket_acl := backblaze.AllPrivate
+		switch bucket.Spec.Acl {
+		case "private":
+			bucket_acl = backblaze.AllPrivate
+		case "public":
+			bucket_acl = backblaze.AllPublic
+		}
+		// Checking if bucket doesnt already exist
+		bucket_b2_exist, bucket_b2_exist_err := b2.Bucket(bucket.Name)
 
-// 	// Update the existing deployment
-// 	depl.Spec.Replicas = &app.Spec.Replicas
-// 	depl.Spec.Template.Spec.Containers[0].Image = app.Spec.Image
-// 	depl.Spec.Template.Spec.Containers[0].Ports[0].ContainerPort = app.Spec.Port
-// 	if err := r.Update(ctx, &depl); err != nil {
-// 		return fmt.Errorf("Unable to update Deployment: %v", err)
-// 	}
+		if bucket_b2_exist_err != nil {
+			return fmt.Errorf("Unable to fetch Bucket: %v", bucket_b2_exist_err)
+		}
 
-// 	return nil
-// }
+		if bucket_b2_exist == nil {
+			log.Info("Creating Bucket")
+			// Creating Bucket
+			bucket_b2, bucket_err := b2.CreateBucket(bucket.Name, bucket_acl)
+
+			if bucket_err != nil {
+				return fmt.Errorf("Unable to create Bucket: %v", bucket_err)
+			} else {
+				bucket.Status.AtProvider = true
+			}
+
+			fmt.Println(string(bucket_b2.BucketType))
+		} else {
+			log.Info("Bucket exist at provider, adopting")
+			bucket.Status.AtProvider = true
+		}
+
+		bucket.Status.Reconciled = true
+		r.Status().Update(ctx, bucket)
+
+		return nil
+	}
+
+	return nil
+}
 
 func (r *BucketReconciler) reconcileDelete(ctx context.Context, bucket *b2v1alpha1.Bucket) (ctrl.Result, error) {
 	log := r.Log.WithValues("bucket", bucket.Namespace)
@@ -178,17 +174,22 @@ func (r *BucketReconciler) reconcileDelete(ctx context.Context, bucket *b2v1alph
 	bucket_b2, _ := b2.Bucket(bucket.Name)
 	if bucket_b2 == nil {
 		log.Info("Bucket not found")
-	}
-
-	// Deleting bucket
-	err := bucket_b2.Delete()
-	if err != nil {
-		log.Info("Error occured while trying to remove bucket (is the bucket empty?)")
-	} else {
 		// Remove the finalizer and update the object
 		controllerutil.RemoveFinalizer(bucket, bucketFinalizer)
 		if err := r.Update(ctx, bucket); err != nil {
 			return ctrl.Result{}, fmt.Errorf("Error removing finalizer: %v", err)
+		}
+	} else {
+		// Deleting bucket
+		err := bucket_b2.Delete()
+		if err != nil {
+			log.Info("Error occured while trying to remove bucket (is the bucket empty?)")
+		} else {
+			// Remove the finalizer and update the object
+			controllerutil.RemoveFinalizer(bucket, bucketFinalizer)
+			if err := r.Update(ctx, bucket); err != nil {
+				return ctrl.Result{}, fmt.Errorf("Error removing finalizer: %v", err)
+			}
 		}
 	}
 
